943019bb52e37d3111694e083fbee3af58a85066
LeDangHoc <ledanghoc1102@gmail.com>
2022-05-28 16:21:51 +0700

    add future

---
+++ Areas/Admin/Models/SpViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ShopDT.Models;
+
+namespace ShopDT.Areas.Admin.Models
+{
+    public class SpViewModel
+    {
+        QLDienThoaiContext db = new QLDienThoaiContext();
+        public Sanpham sp { get; set; }
+        public SelectList Danhmuc { get; set; }
+        public SpViewModel()
+        {
+            this.Danhmuc = new SelectList(db.Danhmucsps, "MaDanhMuc", "TenDanhMuc");
+        }
+    }
+}
